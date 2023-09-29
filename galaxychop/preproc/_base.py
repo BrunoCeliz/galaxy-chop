96e44d7c2e75c82c618d89662f76af73633dea06
Bruno Celiz <bruno.celiz@mi.unc.edu.ar>
2023-09-29 16:23:15 -0300

    Creación de archivos a escribir

---
+++ galaxychop/preproc/_base.py
@@ -0,0 +1,5 @@
+#Bruno:
+# Supongo que uno de los scopes de este glow-up a GlxChop \
+# tiene que ver con repetir el procedimiento del _base.py \
+# de la carpeta /models. Así que acá debería ~recrear esa \
+# carpeta.
---
+++ galaxychop/preproc/grispy_potential.py
@@ -0,0 +1,4 @@
+#Bruno:
+# Para comenzar a hacer algo, veamos la implementación \
+# del potencial con GriSPy (aunqeu ya esté testeado que \
+# no es el algoirtmo más rápido ni más preciso...)

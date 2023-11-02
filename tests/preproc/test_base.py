d4d8c2ff29966c7fa9d6d86180c951a3b7f5c1cd
Bruno Celiz <bruno.celiz@mi.unc.edu.ar>
2023-11-02 10:36:32 -0300

    New branch to make the preproc/ tests.

--- tests/preproc/test_base.py
+++ tests/preproc/test_base.py
@@ -6,6 +6,55 @@
 
 # WIP
 
+import galaxychop as gchop
+
+import numpy as np
+
+import pandas as pd
+
+import pytest
+
+
 # =============================================================================
-# IMPORTS
+# TRANSFORMER ABC
 # =============================================================================
+
+# Bruno:
+# Ojo cómo resulta el match entre lo del test_base de models/...
+@pytest.mark.model
+def GalaxyTransformerABC_not_implemethed():
+    class Transformer(gchop.preproc.GalaxyTransformerABC):
+        def transform(self, galaxy):
+            return super().transform(galaxy)
+
+        def checker(self, galaxy):
+            return super().checker(galaxy)
+
+    transformer = Transformer()
+
+    with pytest.raises(NotImplementedError):
+        transformer.transform(None)
+
+    with pytest.raises(NotImplementedError):
+        transformer.checker(None)
+
+
+@pytest.mark.model
+def test_GalaxyTransformerABC_repr():
+    class Transformer(gchop.preproc.GalaxyTransformerABC):
+        other = gchop.preproc.hparam(default=1)
+
+        def transform(self, galaxy):
+            ...
+
+        def checker(self, galaxy):
+            ...
+
+    transformer = Transformer(other="zaraza")
+    result = repr(transformer)
+    expected = "Transformer(other='zaraza')"
+    # Bruno: Ojo con esto...
+
+    assert result == expected
+
+

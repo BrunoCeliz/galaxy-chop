6fd92d1872fdef1f58f31ef2f51a3313da723235
Bruno Celiz <bruno.celiz@mi.unc.edu.ar>
2023-11-02 11:09:21 -0300

    Done (maybe) preproc/test_base.py

--- tests/preproc/test_base.py
+++ tests/preproc/test_base.py
@@ -58,3 +58,51 @@ def test_GalaxyTransformerABC_repr():
     assert result == expected
 
 
+@pytest.mark.model
+def test_GalaxyTransformerABC_transform(read_hdf5_galaxy):
+    gal = read_hdf5_galaxy("gal394242.h5")
+    # Bruno: Claro, no puedo centrarla/alinearla porque
+    # eso es lo que quiero. Así que CREAMOS un
+    # transformador que, por ejemplo, "aplaste" la glx en
+    # el plano z=0 (!)
+    center = gchop.preproc.Centralizer()
+    gal = center.transform(gal)
+
+    # Bruno: El test consiste en que un transf (no importa
+    # cuál) devuelva una galaxia con la misma estructura que
+    # la que se come (misma cant de parts, length, etc...).
+    class Transformer(gchop.preproc.GalaxyTransformerABC):
+        def transform(self, galaxy):
+            df = galaxy.to_dataframe(attributes=["ptypev","z"])
+            df.loc[:,'z'] = np.zeros(len(df))
+            # Bruno: Copio...
+            stars = df[df.ptypev == gchop.core.data.ParticleSetType.STARS.value]
+            dark_matter = df[df.ptypev == gchop.core.data.ParticleSetType.DARK_MATTER.value]
+            gas = df[df.ptypev == gchop.core.data.ParticleSetType.GAS.value]
+            new = galaxy.disassemble()
+            new.update(
+                z_s=stars.z.to_numpy(),
+                z_dm=dark_matter.z.to_numpy(),
+                z_g=gas.z.to_numpy(),
+            )
+            return gchop.core.data.mkgalaxy(**new)
+
+        def checker(self, galaxy):
+            # Tiene que revisar si todas las partículas tienen z=0
+            gal_transf = self.transform(galaxy)
+            df = gal_transf.to_dataframe(attributes=["ptypev","z"])
+
+            return np.allclose(df.loc[:,'z'], 0, rtol=1e-05, atol=1e-08)
+
+    transformer = Transformer()
+    gal_transf = transformer.transform(gal)
+    is_transf = transformer.checker(gal_transf)
+
+    assert len(gal_transf.stars) == len(gal.stars)
+    assert len(gal_transf.stars) == len(gal.dark_matter)
+    assert len(gal_transf.stars) == len(gal.gas)
+    # Bruno: Ojo con esto. igual está bien que el "checker" debe dar True
+    # para cuando al galaxia sea tranformada...
+    assert is_transf == True
+
+# Bruno: ¿Y hparam()?
